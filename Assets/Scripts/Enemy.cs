c7c2b38486d873c77be4f2bacdc31f189df76aa6
Synx0 <games@rory.es>
2020-03-25 05:10:28 +0100

    Enemy fire implemented. needs refinement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,18 +6,46 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.5f;
+    [SerializeField] float maxTimeBetweenShots = 4f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float laserSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
     }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {                       
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+                      enemyLaserPrefab,
+                     transform.position - new Vector3 (0, 2, 0),
+                     Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -laserSpeed);
+
+
+    }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
